2b4ec5a0d1cec649fdd077d5d4b02b0b8b98a6c5
Joshua <e0544333@u.nus.edu>
2022-02-19 15:21:09 +0800

    Lab2 part1 and part2 complete

--- Lab02/part1/autograder/grade.sh
+++ Lab02/part1/autograder/grade.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
 
 # Check if we have enough arguments
-
+if [ $# -ne 1 ]; then
+echo "Usage: ./grade.sh <filename>"
+    exit 1
+fi
 # Delete temporary files
+rm ref/*.out
 
 # Compile the reference program
+gcc ref/utils.c ref/$1.c -o ref/$1.out
 
 # Generate reference output files
+for i in ref/*.in; do
+    ref/$1.out < $i > $i.out
+done
 
 # Now mark submissions
 
@@ -21,4 +29,52 @@
     # Compare with reference output files  and award 1 mark if they are identical
 # print score for student
 # print total files marked.
+
+# Iterate through /sub content
+echo -n > results.out
+echo -e Test date and time: $(date +%A), $(date +%d\ %B\ %Y), $(date +%T) "\n" >> results.out 
+score=0
+# Find the total number of .in files in ref/ subdirectory
+len=$(find ref/ -type f -name "*.in" | wc -l)
+total_student_files=0
+
+for i in subs/*; do
+    # Compile C code
+    gcc $i/utils.c $i/$1.c -o $i/$1.out
+    if [ $? -ne 0 ]; then
+        # Student gets 0 score
+        # Print compile error message to output file
+        echo Directory $(basename $i) has a compile error. >> results.out
+    else
+        # Generate output from C code using *.in files in ref
+        file_counter=1
+        for j in ref/*.in; do
+            $i/$1.out < $j > $i/s$file_counter.out
+            let file_counter=file_counter+1
+        done
+
+        # Compare with reference output files  and award 1 mark if they are identical
+        file_counter=1
+        
+        while [[ $file_counter -le $len ]]; do
+            # ref/s$counter.in.out $i/s$counter.out
+            diff --brief <(sort ref/s$file_counter.in.out) <(sort $i/s$file_counter.out)
+            comp_value=$?
+
+            if [ $comp_value -eq 0 ]; then
+                # do something because they are identical
+                let score=score+1
+            fi
+            let file_counter=file_counter+1
+        done
+    fi
+    # print score for student
+    echo Directory $(basename $i) score $score/$len >> results.out
     
+    # Reset score to 0
+    score=0
+    # Increment total student submissions by 1
+    let total_student_files=total_student_files+1 
+done
+# print total files marked.
+echo -e "\n"Processed $total_student_files files. >> results.out
--- Lab02/part2/lab2p2f.c
+++ Lab02/part2/lab2p2f.c
@@ -4,7 +4,8 @@
 #include <sys/wait.h>
 #include <fcntl.h>
 
-int main() {
+int main()
+{
 
     printf("Be patient, the program will take around 7 seconds to run.\n");
     printf("At the end you can do \"cat results.out\" to see the result.\n");
@@ -16,5 +17,33 @@ int main() {
     // WITHOUT using | and > from the shell.
     //
 
-}
+    // pipefd[0] Read end: Child is using the read end.
+    // pipefd[1] Write end: Parent is using the write end.
+    int pipefd[2];
 
+    if (pipe(pipefd) < 0)
+    {
+        // The "perror" call prints the cause of the error to the screen
+        perror("lab2p2f: ");
+    }
+    // Parent
+    if (fork() != 0)
+    {
+        wait(NULL); // Wait for child
+        int fp_out = open("./results.out", O_CREAT | O_WRONLY);
+        close(pipefd[1]); // The end we are not using.
+        dup2(pipefd[0], STDIN_FILENO);
+        dup2(fp_out, STDOUT_FILENO);
+        close(pipefd[0]);
+        execlp("./talk", "talk", (char *)0);
+        close(fp_out);
+    }
+    // Child
+    else
+    {
+        close(pipefd[0]); // The the end we are not using.
+        dup2(pipefd[1], STDOUT_FILENO);
+        close(pipefd[1]);
+        execl("./slow", "slow", "5", NULL);
+    }
+}
